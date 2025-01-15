e352033cb99a6722e4729a0075066ec59990e65c
KiraCodes6103 <21je1072@iitism.ac.in>
2025-01-15 11:40:59 +0530

    Basic Layout Done

---
+++ src/app/(protected)/app-sidebar.tsx
@@ -0,0 +1,148 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import { cn } from "@/lib/utils";
+import {
+  Bot,
+  CreditCard,
+  LayoutDashboard,
+  Plus,
+  Presentation,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+const items = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Q&A",
+    url: "/qa",
+    icon: Bot,
+  },
+  {
+    title: "Meetings",
+    url: "/meeting",
+    icon: Presentation,
+  },
+  {
+    title: "Billing",
+    url: "/billing",
+    icon: CreditCard,
+  },
+];
+const projects = [
+  {
+    name: "Project 1",
+  },
+  {
+    name: "Project 2",
+  },
+  {
+    name: "Project 3",
+  },
+  {
+    name: "Project 4",
+  },
+];
+export const AppSidebar = () => {
+  const pathName = usePathname();
+  const { open } = useSidebar();
+  return (
+    <Sidebar collapsible="icon" variant="floating">
+      <SidebarHeader>
+        <div className="flex items-center gap-2">
+          <Image src="/logo.png" alt="Logo" height={40} width={40} />
+          {open && (
+            <div className="text-xl font-bold text-primary/80">GitSage</div>
+          )}
+        </div>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => {
+                return (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <Link
+                        href={item.url}
+                        className={cn({
+                          "!bg-primary !text-white": pathName === item.url,
+                        })}
+                      >
+                        {/* cn => conditional class addition where keys are the class and value is the condition */}
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Your Projects</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {projects.map((project) => {
+                return (
+                  <SidebarMenuItem key={project.name}>
+                    <SidebarMenuButton asChild>
+                      <div>
+                        <div
+                          className={cn(
+                            "item-center flex size-6 justify-center rounded-sm border bg-white text-sm text-primary",
+                            // {
+                            //     "bg-primary text-white" : project.name === project.id
+                            // },
+                            {
+                              "bg-primary text-white": true,
+                            },
+                          )}
+                        >
+                          {project.name[0]}
+                        </div>
+                        <span>{project.name}</span>
+                      </div>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
+              <SidebarMenuItem>
+                <Link href="/create">
+                  <div className="h-2">
+                    <Button variant={"outline"} size="sm" className="w-fit">
+                      <Plus />
+                      {open && "Create Project"}
+                    </Button>
+                  </div>
+                </Link>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+};
