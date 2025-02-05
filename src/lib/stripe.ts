9e9b9b3fc32be215e6b82a4e4114d8bf92126dec
KiraCodes6103 <21je1072@iitism.ac.in>
2025-02-05 11:22:37 +0530

    Billing Page UI created and stripe is configured

---
+++ src/lib/stripe.ts
@@ -0,0 +1,43 @@
+"use server";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2025-01-27.acacia",
+});
+
+export const createCheckoutSession = async (credits: number) => {
+  const successUrl = `${process.env.NEXT_PUBLIC_APP_URL}create`;
+  const cancelUrl = `${process.env.NEXT_PUBLIC_APP_URL}dashboard`;
+
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("unauthorized");
+  }
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "inr",
+          product_data: {
+            name: `${credits} GitSage Credits`,
+          },
+          unit_amount: Math.round((credits * 2) * 100),
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    customer_creation: "always",
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+    client_reference_id: userId.toString(),
+    metadata: {
+      credits,
+    },
+  });
+
+  return redirect(session.url!);
+};
